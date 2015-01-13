47762b91d817ae0beb295520a5274d72511fafca
ChrisD90 <christoph.doeringer+github@gmail.com>
2015-01-13 08:58:30 +0100

    second commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
+import view.Window_Main;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("TEST");
+		Window_Main wm = new Window_Main();
 
 	}
 
---
+++ src/view/Window_Main.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class Window_Main extends JFrame{
+
+	public Window_Main() {
+		setSize(100, 100);
+		setVisible(true);
+	}
+}
