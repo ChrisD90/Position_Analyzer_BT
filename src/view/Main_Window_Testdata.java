a31d17bffeaf617c551823d85aa112204e15e0ec
ChrisD90 <Christoph@Chris>
2015-02-25 14:09:34 +0100

    - update

--- src/view/Main_Window_Testdata.java
+++ src/view/Main_Window_Testdata.java
@@ -26,30 +26,20 @@ import java.awt.Toolkit;
 import java.awt.SystemColor;
 
 import javax.swing.JSeparator;
+import java.awt.Font;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
 
 public class Main_Window_Testdata extends JFrame {
 
 	private JPanel contentPane;
 	public int[] values = new int[17];
 	private JSpinner spinner;
-	private JSpinner spinner_1;
-	private JSpinner spinner_2;
-	private JSpinner spinner_3;
-	private JSpinner spinner_4;
-	private JSpinner spinner_5;
-	private JSpinner spinner_6;
-	private JSpinner spinner_7;
-	private JSpinner spinner_8;
-	private JSpinner spinner_9;
-	private JSpinner spinner_10;
-	private JSpinner spinner_11;
-	private JSpinner spinner_12;
-	private JSpinner spinner_13;
-	private JSpinner spinner_14;
-	private JSpinner spinner_15;
-	private JSpinner spinner_16;
 
 	Player player;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Create the frame.
@@ -68,156 +58,13 @@ public class Main_Window_Testdata extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JDesktopPane desktopPane = new JDesktopPane();
-		desktopPane.setBackground(Color.LIGHT_GRAY);
-		desktopPane.setBounds(350, 30, 434, 420);
-		contentPane.add(desktopPane);
-
-		// ####################SPINNER####################
-
-		JLabel lblAusdauer = new JLabel("Ausdauer (gelaufene km)");
-		lblAusdauer.setBounds(10, 39, 236, 14);
+		JLabel lblAusdauer = new JLabel("Yo-Yo Test (in m)");
+		lblAusdauer.setBounds(10, 60, 236, 14);
 		contentPane.add(lblAusdauer);
 
 		spinner = new JSpinner();
-		spinner.setBounds(256, 30, 53, 20);
+		spinner.setBounds(256, 57, 53, 20);
 		contentPane.add(spinner);
-
-		JLabel lblAnzahlDerSprints = new JLabel("Anzahl der Sprints");
-		lblAnzahlDerSprints.setBounds(10, 64, 236, 14);
-		contentPane.add(lblAnzahlDerSprints);
-
-		spinner_1 = new JSpinner();
-		spinner_1.setBounds(256, 55, 53, 20);
-		contentPane.add(spinner_1);
-
-		JLabel lblDurchscnSprintsrecke = new JLabel("Durchschn. Sprintstrecke");
-		lblDurchscnSprintsrecke.setBounds(10, 89, 236, 14);
-		contentPane.add(lblDurchscnSprintsrecke);
-
-		spinner_2 = new JSpinner();
-		spinner_2.setBounds(256, 80, 53, 20);
-		contentPane.add(spinner_2);
-
-		JLabel lblSprungkraftinCm = new JLabel("Sprungkraft (in cm)");
-		lblSprungkraftinCm.setBounds(10, 114, 236, 14);
-		contentPane.add(lblSprungkraftinCm);
-
-		spinner_3 = new JSpinner();
-		spinner_3.setBounds(256, 105, 53, 20);
-		contentPane.add(spinner_3);
-
-		JLabel lblZweikmpfegesamt = new JLabel("Zweik\u00E4mpfe (gesamt)");
-		lblZweikmpfegesamt.setBounds(10, 139, 236, 14);
-		contentPane.add(lblZweikmpfegesamt);
-
-		spinner_4 = new JSpinner();
-		spinner_4.setBounds(256, 130, 53, 20);
-		contentPane.add(spinner_4);
-
-		JLabel lblZweikmpfeAmBoden = new JLabel(
-				"Zweik\u00E4mpfe am Boden (gewonnen)");
-		lblZweikmpfeAmBoden.setBounds(10, 161, 236, 14);
-		contentPane.add(lblZweikmpfeAmBoden);
-
-		spinner_5 = new JSpinner();
-		spinner_5.setBounds(256, 155, 53, 20);
-		contentPane.add(spinner_5);
-
-		JLabel lblZweikmpfeAmBoden_1 = new JLabel(
-				"Zweik\u00E4mpfe am Boden (verloren)");
-		lblZweikmpfeAmBoden_1.setBounds(10, 186, 236, 14);
-		contentPane.add(lblZweikmpfeAmBoden_1);
-
-		spinner_6 = new JSpinner();
-		spinner_6.setBounds(256, 180, 53, 20);
-		contentPane.add(spinner_6);
-
-		JLabel lblZweikmpfeInDer = new JLabel(
-				"Zweik\u00E4mpfe in der Luft (gewonnen)");
-		lblZweikmpfeInDer.setBounds(10, 211, 236, 14);
-		contentPane.add(lblZweikmpfeInDer);
-
-		spinner_7 = new JSpinner();
-		spinner_7.setBounds(256, 205, 53, 20);
-		contentPane.add(spinner_7);
-
-		JLabel lblZweikmpfeInDer_1 = new JLabel(
-				"Zweik\u00E4mpfe in der Luft (verloren)");
-		lblZweikmpfeInDer_1.setBounds(10, 236, 236, 14);
-		contentPane.add(lblZweikmpfeInDer_1);
-
-		spinner_8 = new JSpinner();
-		spinner_8.setBounds(256, 230, 53, 20);
-		contentPane.add(spinner_8);
-
-		JLabel lblPssegesamt = new JLabel("P\u00E4sse (gesamt)");
-		lblPssegesamt.setBounds(10, 261, 236, 14);
-		contentPane.add(lblPssegesamt);
-
-		spinner_9 = new JSpinner();
-		spinner_9.setBounds(256, 258, 53, 20);
-		contentPane.add(spinner_9);
-
-		JLabel lblPsseangekommen = new JLabel("P\u00E4sse (angekommen)");
-		lblPsseangekommen.setBounds(10, 286, 236, 14);
-		contentPane.add(lblPsseangekommen);
-
-		spinner_10 = new JSpinner();
-		spinner_10.setBounds(256, 283, 53, 20);
-		contentPane.add(spinner_10);
-
-		JLabel lblPssekurzeDistanz = new JLabel("P\u00E4sse (kurze Distanz");
-		lblPssekurzeDistanz.setBounds(10, 311, 236, 14);
-		contentPane.add(lblPssekurzeDistanz);
-
-		spinner_11 = new JSpinner();
-		spinner_11.setBounds(256, 308, 53, 20);
-		contentPane.add(spinner_11);
-
-		JLabel lblDribblingzeitIm = new JLabel(
-				"Dribbling (Zeit in sec. im Parcour)");
-		lblDribblingzeitIm.setBounds(10, 336, 236, 14);
-		contentPane.add(lblDribblingzeitIm);
-
-		spinner_12 = new JSpinner();
-		spinner_12.setBounds(256, 333, 53, 20);
-		contentPane.add(spinner_12);
-
-		JLabel lblSpielverstndnis = new JLabel("Spielverst\u00E4ndnis (1 - 6)");
-		lblSpielverstndnis.setBounds(10, 361, 236, 14);
-		contentPane.add(lblSpielverstndnis);
-
-		spinner_13 = new JSpinner();
-		spinner_13.setModel(new SpinnerNumberModel(1, 1, 6, 1));
-		spinner_13.setBounds(256, 358, 53, 20);
-		contentPane.add(spinner_13);
-
-		JLabel lblFlankengesamt = new JLabel("Flanken (gesamt)");
-		lblFlankengesamt.setBounds(10, 386, 236, 14);
-		contentPane.add(lblFlankengesamt);
-
-		spinner_14 = new JSpinner();
-		spinner_14.setBounds(256, 383, 53, 20);
-		contentPane.add(spinner_14);
-
-		JLabel lblFlankenerfolgreich = new JLabel("Flanken (erfolgreich)");
-		lblFlankenerfolgreich.setBounds(10, 411, 236, 14);
-		contentPane.add(lblFlankenerfolgreich);
-
-		spinner_15 = new JSpinner();
-		spinner_15.setBounds(256, 408, 53, 20);
-		contentPane.add(spinner_15);
-
-		JLabel lblSchsseAufsTor = new JLabel("Sch\u00FCsse aufs Tor");
-		lblSchsseAufsTor.setBounds(10, 436, 236, 14);
-		contentPane.add(lblSchsseAufsTor);
-
-		spinner_16 = new JSpinner();
-		spinner_16.setBounds(256, 433, 53, 20);
-		contentPane.add(spinner_16);
-
-		// ####################MENU####################
 
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 794, 21);
@@ -228,75 +75,288 @@ public class Main_Window_Testdata extends JFrame {
 
 		JMenuItem mntmClose = new JMenuItem("Close");
 		mnFile.add(mntmClose);
-		mntmClose.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-
-			}
-		});
 
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
-		
+
 		JMenuItem mntmCheatSheet = new JMenuItem("Cheat Sheet");
 		mnHelp.add(mntmCheatSheet);
-		
+
 		JSeparator separator = new JSeparator();
 		mnHelp.add(separator);
-		
+
 		JMenuItem mntmAbout = new JMenuItem("About");
 		mnHelp.add(mntmAbout);
+
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.setBounds(673, 507, 106, 37);
+		contentPane.add(btnSubmit);
+
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(15, 515, 115, 29);
+		contentPane.add(btnBack);
+
+		JLabel lblAusdauerfhigkeiten = new JLabel("Ausdauerf\u00E4higkeiten");
+		lblAusdauerfhigkeiten.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblAusdauerfhigkeiten.setBounds(10, 35, 236, 14);
+		contentPane.add(lblAusdauerfhigkeiten);
+
+		JLabel lblSchnelligkeitsfhigkeiten = new JLabel(
+				"Schnelligkeitsf\u00E4higkeiten");
+		lblSchnelligkeitsfhigkeiten.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblSchnelligkeitsfhigkeiten.setBounds(10, 95, 299, 14);
+		contentPane.add(lblSchnelligkeitsfhigkeiten);
+
+		JLabel lblmZeit = new JLabel("5m - Zeit in 1/10s");
+		lblmZeit.setBounds(10, 120, 236, 14);
+		contentPane.add(lblmZeit);
+
+		JLabel lblNewLabel_1 = new JLabel("10m - Zeit in 1/10s");
+		lblNewLabel_1.setBounds(10, 145, 236, 14);
+		contentPane.add(lblNewLabel_1);
+
+		JLabel lblmZeit_1 = new JLabel("30m - Zeit in 1/10s");
+		lblmZeit_1.setBounds(10, 170, 236, 14);
+		contentPane.add(lblmZeit_1);
+
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setBounds(256, 117, 53, 20);
+		contentPane.add(spinner_1);
+
+		JSpinner spinner_2 = new JSpinner();
+		spinner_2.setBounds(256, 142, 53, 20);
+		contentPane.add(spinner_2);
+
+		JSpinner spinner_3 = new JSpinner();
+		spinner_3.setBounds(256, 167, 53, 20);
+		contentPane.add(spinner_3);
+
+		JLabel lblSprungkraft = new JLabel("Sprungkraft");
+		lblSprungkraft.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblSprungkraft.setBounds(10, 205, 236, 14);
+		contentPane.add(lblSprungkraft);
+
+		JLabel lblErgebnisJump = new JLabel("Ergebnis - Jump & Reach (in cm)");
+		lblErgebnisJump.setBounds(10, 230, 236, 14);
+		contentPane.add(lblErgebnisJump);
+
+		JSpinner spinner_4 = new JSpinner();
+		spinner_4.setBounds(256, 227, 53, 20);
+		contentPane.add(spinner_4);
+
+		JLabel lblPassspiel = new JLabel("Passspiel");
+		lblPassspiel.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblPassspiel.setBounds(10, 265, 236, 14);
+		contentPane.add(lblPassspiel);
+
+		JLabel lblKurzePsse = new JLabel("kurze P\u00E4sse - total");
+		lblKurzePsse.setBounds(10, 290, 236, 14);
+		contentPane.add(lblKurzePsse);
+
+		JLabel lblLangePsse = new JLabel("lange P\u00E4sse - total");
+		lblLangePsse.setBounds(10, 315, 236, 14);
+		contentPane.add(lblLangePsse);
+
+		JLabel lblKurzePsse_1 = new JLabel("kurze P\u00E4sse - erfolgreich");
+		lblKurzePsse_1.setBounds(10, 340, 236, 14);
+		contentPane.add(lblKurzePsse_1);
+
+		JLabel lblLangePsseErfolgreich = new JLabel(
+				"lange P\u00E4sse - erfolgreich");
+		lblLangePsseErfolgreich.setBounds(10, 365, 236, 14);
+		contentPane.add(lblLangePsseErfolgreich);
+
+		JSpinner spinner_5 = new JSpinner();
+		spinner_5.setBounds(256, 287, 53, 20);
+		contentPane.add(spinner_5);
+
+		JSpinner spinner_6 = new JSpinner();
+		spinner_6.setBounds(256, 312, 53, 20);
+		contentPane.add(spinner_6);
+
+		JSpinner spinner_7 = new JSpinner();
+		spinner_7.setBounds(256, 337, 53, 20);
+		contentPane.add(spinner_7);
+
+		JSpinner spinner_8 = new JSpinner();
+		spinner_8.setBounds(256, 362, 53, 20);
+		contentPane.add(spinner_8);
+
+		JLabel lblFlanken = new JLabel("Flanken");
+		lblFlanken.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblFlanken.setBounds(10, 400, 236, 14);
+		contentPane.add(lblFlanken);
+
+		JLabel lblFlankenTotal = new JLabel("Flanken - total");
+		lblFlankenTotal.setBounds(10, 425, 236, 14);
+		contentPane.add(lblFlankenTotal);
+
+		JLabel lblFlankenAngekommen = new JLabel("Flanken - angekommen");
+		lblFlankenAngekommen.setBounds(10, 450, 236, 14);
+		contentPane.add(lblFlankenAngekommen);
+
+		JSpinner spinner_9 = new JSpinner();
+		spinner_9.setBounds(256, 422, 53, 20);
+		contentPane.add(spinner_9);
+
+		JSpinner spinner_10 = new JSpinner();
+		spinner_10.setBounds(256, 447, 53, 20);
+		contentPane.add(spinner_10);
+
+		Component verticalStrut = Box.createVerticalStrut(20);
+		verticalStrut.setBounds(401, 32, 12, 461);
+		contentPane.add(verticalStrut);
+
+		JLabel lblDribbling = new JLabel("Dribbling");
+		lblDribbling.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblDribbling.setBounds(423, 36, 236, 14);
+		contentPane.add(lblDribbling);
+
+		JLabel lblmZeit_2 = new JLabel("5m - Zeit in 1/10s");
+		lblmZeit_2.setBounds(423, 60, 236, 14);
+		contentPane.add(lblmZeit_2);
+
+		JLabel lblmZeit_3 = new JLabel("10m - Zeit in 1/10s");
+		lblmZeit_3.setBounds(423, 85, 236, 14);
+		contentPane.add(lblmZeit_3);
+
+		JLabel lblmZeit_4 = new JLabel("30m - Zeit in 1/10s");
+		lblmZeit_4.setBounds(423, 110, 236, 14);
+		contentPane.add(lblmZeit_4);
+
+		JSpinner spinner_11 = new JSpinner();
+		spinner_11.setBounds(669, 57, 53, 20);
+		contentPane.add(spinner_11);
+
+		JSpinner spinner_12 = new JSpinner();
+		spinner_12.setBounds(669, 82, 53, 20);
+		contentPane.add(spinner_12);
+
+		JSpinner spinner_13 = new JSpinner();
+		spinner_13.setBounds(669, 107, 53, 20);
+		contentPane.add(spinner_13);
+
+		JLabel lblTorschuss = new JLabel("Torschuss");
+		lblTorschuss.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblTorschuss.setBounds(423, 145, 236, 14);
+		contentPane.add(lblTorschuss);
+
+		JLabel lblSchsseAufsTor = new JLabel("Sch\u00FCsse aufs Tor - total");
+		lblSchsseAufsTor.setBounds(423, 170, 236, 14);
+		contentPane.add(lblSchsseAufsTor);
+
+		JLabel lblSchsseAufsTor_1 = new JLabel(
+				"Sch\u00FCsse aufs Tor - erfolgreich");
+		lblSchsseAufsTor_1.setBounds(423, 195, 236, 14);
+		contentPane.add(lblSchsseAufsTor_1);
+
+		JSpinner spinner_14 = new JSpinner();
+		spinner_14.setBounds(673, 167, 49, 20);
+		contentPane.add(spinner_14);
+
+		JSpinner spinner_15 = new JSpinner();
+		spinner_15.setBounds(673, 192, 49, 20);
+		contentPane.add(spinner_15);
+
+		JLabel lblZweikampfverhalten = new JLabel("Zweikampfverhalten");
+		lblZweikampfverhalten.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblZweikampfverhalten.setBounds(423, 230, 236, 14);
+		contentPane.add(lblZweikampfverhalten);
+
+		JLabel lblOffensiveZweikmpfe = new JLabel(
+				"Offensive Zweik\u00E4mpfe - gewonnen");
+		lblOffensiveZweikmpfe.setBounds(423, 255, 236, 14);
+		contentPane.add(lblOffensiveZweikmpfe);
+
+		JLabel lblDefensiveZweikmpfe = new JLabel(
+				"Defensive Zweik\u00E4mpfe - gewonnen");
+		lblDefensiveZweikmpfe.setBounds(423, 280, 236, 14);
+		contentPane.add(lblDefensiveZweikmpfe);
+
+		JSpinner spinner_16 = new JSpinner();
+		spinner_16.setBounds(673, 252, 49, 20);
+		contentPane.add(spinner_16);
+
+		JSpinner spinner_17 = new JSpinner();
+		spinner_17.setBounds(673, 277, 49, 20);
+		contentPane.add(spinner_17);
+
+		JLabel lblSpielverstndnis = new JLabel("Spielverst\u00E4ndnis");
+		lblSpielverstndnis.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblSpielverstndnis.setBounds(423, 315, 237, 14);
+		contentPane.add(lblSpielverstndnis);
+
+		JLabel lblEinschtzungAlsSchulnote = new JLabel(
+				"Einsch\u00E4tzung als Schulnote");
+		lblEinschtzungAlsSchulnote.setBounds(423, 340, 236, 14);
+		contentPane.add(lblEinschtzungAlsSchulnote);
+
+		JSpinner spinner_18 = new JSpinner();
+		spinner_18.setModel(new SpinnerNumberModel(1, 1, 6, 1));
+		spinner_18.setBounds(673, 337, 49, 20);
+		contentPane.add(spinner_18);
+
+		JLabel lblFuarbeit = new JLabel("Fu\u00DFarbeit");
+		lblFuarbeit.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblFuarbeit.setBounds(423, 380, 236, 14);
+		contentPane.add(lblFuarbeit);
+
+		JRadioButton rdbtnEherLinks = new JRadioButton("eher links");
+		buttonGroup.add(rdbtnEherLinks);
+		rdbtnEherLinks.setBounds(419, 397, 109, 23);
+		contentPane.add(rdbtnEherLinks);
+
+		JRadioButton rdbtnEherRechts = new JRadioButton("eher rechts");
+		buttonGroup.add(rdbtnEherRechts);
+		rdbtnEherRechts.setBounds(530, 397, 109, 23);
+		contentPane.add(rdbtnEherRechts);
+
+		JRadioButton rdbtnBeidfig = new JRadioButton("beidf\u00FC\u00DFig");
+		buttonGroup.add(rdbtnBeidfig);
+		rdbtnBeidfig.setBounds(641, 397, 109, 23);
+		contentPane.add(rdbtnBeidfig);
+
+		initActionListeners(mntmClose, mntmCheatSheet, mntmAbout, btnSubmit,
+				btnBack);
+	}
+
+	/**
+	 * @param mntmClose
+	 * @param mntmCheatSheet
+	 * @param mntmAbout
+	 * @param btnSubmit
+	 * @param btnBack
+	 */
+	private void initActionListeners(JMenuItem mntmClose,
+			JMenuItem mntmCheatSheet, JMenuItem mntmAbout, JButton btnSubmit,
+			JButton btnBack) {
+		btnBack.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Start_Window sw = new Start_Window();
+				sw.setVisible(true);
+				dispose();
+			}
+		});
+
 		mntmAbout.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				About_Window aw = new About_Window();
 				aw.setVisible(true);
-				
-			}
-		});
-		
-		mntmCheatSheet.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Help_Window hw = new Help_Window();
-				hw.setVisible(true);
-				
+
 			}
 		});
 
-		// ####################BUTTONS####################
-		
-		JButton btnSubmit = new JButton("Submit");
 		btnSubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				Progress_Bar_Window dialog = new Progress_Bar_Window();
 				dialog.setVisible(true);
-								
 
 				System.out.println("Calculating Optimal Position");
-
-				values[0] = Integer.parseInt(spinner.getValue().toString());
-				values[1] = Integer.parseInt(spinner_1.getValue().toString());
-				values[2] = Integer.parseInt(spinner_2.getValue().toString());
-				values[3] = Integer.parseInt(spinner_3.getValue().toString());
-				values[4] = Integer.parseInt(spinner_4.getValue().toString());
-				values[5] = Integer.parseInt(spinner_5.getValue().toString());
-				values[6] = Integer.parseInt(spinner_6.getValue().toString());
-				values[7] = Integer.parseInt(spinner_7.getValue().toString());
-				values[8] = Integer.parseInt(spinner_8.getValue().toString());
-				values[9] = Integer.parseInt(spinner_9.getValue().toString());
-				values[10] = Integer.parseInt(spinner_10.getValue().toString());
-				values[11] = Integer.parseInt(spinner_11.getValue().toString());
-				values[12] = Integer.parseInt(spinner_12.getValue().toString());
-				values[13] = Integer.parseInt(spinner_13.getValue().toString());
-				values[14] = Integer.parseInt(spinner_14.getValue().toString());
-				values[15] = Integer.parseInt(spinner_15.getValue().toString());
-				values[16] = Integer.parseInt(spinner_16.getValue().toString());
 
 				player = new Player(values);
 				// player.printValues(values);
@@ -305,29 +365,25 @@ public class Main_Window_Testdata extends JFrame {
 				// String inhabits the calculated Positions
 				String string = player.positions;
 
-				desktopPane.setBackground(Color.GREEN);
-				JLabel lblNewLabel = new JLabel(
-						"Perfect Position(s) for Player: " + string);
-				lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-				lblNewLabel.setBounds(141, 142, 200, 97);
-				desktopPane.add(lblNewLabel);
+			}
+		});
+
+		mntmCheatSheet.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Help_Window hw = new Help_Window();
+				hw.setVisible(true);
 
 			}
 		});
 
-		btnSubmit.setBounds(673, 507, 106, 37);
-		contentPane.add(btnSubmit);
-		
-		JButton btnBack = new JButton("Back");
-		btnBack.setBounds(15, 515, 115, 29);
-		contentPane.add(btnBack);
-		btnBack.addActionListener(new ActionListener() {
-			
+		mntmClose.addActionListener(new ActionListener() {
+
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				Start_Window sw = new Start_Window();
-				sw.setVisible(true);
-				dispose();
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+
 			}
 		});
 	}
